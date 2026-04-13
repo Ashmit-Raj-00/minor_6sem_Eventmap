a0f3eace4e866e34d8e911e9f2a31f64eed0be44
Ashmit-Raj-00 <23cse119.ashmitraj@giet.edu>
2026-04-13 23:51:10 +0530

    chore: update config formatting and add .netlify to gitignore

--- web/config.js
+++ web/config.js
@@ -1,5 +1,3 @@
-// This file is overwritten during Netlify builds by `scripts/netlify-build.sh`.
-// For local Go server runs, `/config.js` is served dynamically.
 window.__EVENTMAP_CONFIG__ = {
-  apiBase: "",
+  apiBase: '',
 };
